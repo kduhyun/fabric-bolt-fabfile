48489e2ec3976eb3fb2fcc5461a7ca2ddc0b3fa2
kduhyun <kduhyun@gmail.com>
2016-09-17 03:55:47 +0900

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,13 @@ def deploy():
         run("s3cmd get --force s3://owltree/OwlTree-1.0.0.zip /svc/owltree/ && cd /svc/owltree/ && unzip -o ./OwlTree-1.0.0.zip")
         sudo("ps aux | grep OwlTree-1.0.0.jar | grep -v grep | awk '{print $2}' | xargs kill && sleep 5")
 
+@parallel
+def deployAll():
+    run("echo "+env.host)
+    with settings(warn_only=True):
+        run("s3cmd get --force s3://owltree/OwlTree-1.0.0.zip /svc/owltree/ && cd /svc/owltree/ && unzip -o ./OwlTree-1.0.0.zip")
+        sudo("ps aux | grep OwlTree-1.0.0.jar | grep -v grep | awk '{print $2}' | xargs kill && sleep 5")
+        
 def local():
     build()
     deploy()
