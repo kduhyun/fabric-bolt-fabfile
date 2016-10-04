02bdd97f43d17a4c9c3158e6351997940050f0b5
kduhyun <kduhyun@gmail.com>
2016-10-04 16:28:15 +0900

    Update setup.haproxy.sh

--- master/setup.haproxy.sh
+++ master/setup.haproxy.sh
@@ -1,7 +1,7 @@
 touch /etc/haproxy/haproxy.cfg
 mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old
 cat /etc/haproxy/haproxy.cfg.template > /etc/haproxy/haproxy.cfg
-mysql -u owltree -h owltreedb.internal owltree -e "select concat('    server ','app',instanceId,' ',internalIp,':8080 check') from ServerList" | grep -v concat >> /etc/haproxy/haproxy.cfg
+mysql -u owltree -h owltreedb.internal owltree -e "select concat('    server ','app',instanceId,' ',internalIp,':8080 check') from ServerList where useYN<>'N'" | grep -v concat >> /etc/haproxy/haproxy.cfg
 
 diff=`cmp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old | wc -l`
 
