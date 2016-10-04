f210bb68693b9c074b9eb612d26a126884f63cf9
kduhyun <kduhyun@gmail.com>
2016-10-04 17:37:28 +0900

    Update setup.haproxy.sh

--- master/setup.haproxy.sh
+++ master/setup.haproxy.sh
@@ -1,10 +1,10 @@
 source /home/ec2-user/.bash_profile
 
 rm -f /etc/haproxy/haproxy.cfg.template
-wget https://raw.githubusercontent.com/kduhyun/fabric-bolt-fabfile/master/master/haproxy.cfg.template -O /etc/haproxy/haproxy.cfg.template
+wget --no-cache https://raw.githubusercontent.com/kduhyun/fabric-bolt-fabfile/master/master/haproxy.cfg.template -O /etc/haproxy/haproxy.cfg.template
 
 touch /etc/haproxy/haproxy.cfg
-mv /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old
+mv -f /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.old
 sed s/password/${PASSWORD}/ /etc/haproxy/haproxy.cfg.template > /etc/haproxy/haproxy.cfg
 
 mysql -u owltree -h owltreedb.internal owltree -e "select concat('    server ','app',instanceId,' ',internalIp,':8080 check') from ServerList where useYN<>'N'" | grep -v concat >> /etc/haproxy/haproxy.cfg
