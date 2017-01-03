2ea1dfd6c07558aa84fcbec5df39f05a04cdde85
kduhyun <kduhyun@gmail.com>
2017-01-04 00:32:03 +0900

    Update fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -7,14 +7,11 @@ env.warn_only=True
 def build():
     with settings(warn_only=False):
         run("cd /svc/git/owltreeserver && git pull")
-        mavenResult=run("cd /svc/git/owltreeserver && git checkout develop && git pull && ./mvnw install -Dmaven.test.skip=false")
-        #print("mavenResult: " + mavenResult)
-        print("mavenResult: " + str(mavenResult.return_code))
-        if mavenResult.return_code == 0:
-            run("rm -f /svc/git/owltreeserver/target/OwlTree-1.0.0.zip && zip -9 -j /svc/git/owltreeserver/target/OwlTree-1.0.0.zip /svc/git/owltreeserver/target/OwlTree-1.0.0.jar && s3cmd put --force /svc/git/owltreeserver/target/OwlTree-1.0.0.zip s3://owltree/")
-            sudo("rm -f /svc/shell/setup.haproxy.sh")
-            sudo("wget --no-cache --no-check-certificate https://raw.githubusercontent.com/kduhyun/fabric-bolt-fabfile/master/master/setup.haproxy.sh -O /svc/shell/setup.haproxy.sh")
-            sudo("sh /svc/shell/setup.haproxy.sh")
+        run("cd /svc/git/owltreeserver && git checkout develop && git pull && ./mvnw install -Dmaven.test.skip=false")
+        run("rm -f /svc/git/owltreeserver/target/OwlTree-1.0.0.zip && zip -9 -j /svc/git/owltreeserver/target/OwlTree-1.0.0.zip /svc/git/owltreeserver/target/OwlTree-1.0.0.jar && s3cmd put --force /svc/git/owltreeserver/target/OwlTree-1.0.0.zip s3://owltree/")
+        sudo("rm -f /svc/shell/setup.haproxy.sh")
+        sudo("wget --no-cache --no-check-certificate https://raw.githubusercontent.com/kduhyun/fabric-bolt-fabfile/master/master/setup.haproxy.sh -O /svc/shell/setup.haproxy.sh")
+        sudo("sh /svc/shell/setup.haproxy.sh")
 
 def processDeploying():    
     with settings(warn_only=True):
